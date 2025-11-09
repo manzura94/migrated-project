d395965aebfb9d190ef0c24a74ac68d70a74f083
manzura94 <manzura_jabborova@mail.ru>
2025-11-09 18:41:02 +0500

    init: start migrating the project

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.scss";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Coffee House",
+  description: "Modern Coffee Shop built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
+      <body>
+        <div className="wrapper">
+          <Navbar />
+          <>{children}</>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import AboutSection from "./components/AboutSection";
+import Download from "./components/Download";
+import Favorite from "./components/Favourite";
+import Homepage from "./components/HomePage";
+
+export default function Home() {
+  return (
+    <main className="mainpage">
+      <Homepage />
+      {/* <Favorite/> */}
+      <AboutSection />
+      <Download />
+    </main>
+  );
+}
